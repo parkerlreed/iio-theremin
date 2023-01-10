e64ceaa57cb666f43324f888dc051dd1c123f053
parkerlreed <parker.l.reed@gmail.com>
2023-01-10 18:31:26 -0500

    Handle Ctrl C a bit more gracefully
    Something in Portaudio/libasound still throws some glibc garbage on quit but at least it's consistent now.

--- iio-theremin.py
+++ iio-theremin.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
+import time
+import sys
+import signal
 from pysinewave import SineWave
 
-# Create a sine wave, with a starting pitch of 0, and a pitch change speed of 30/second.
-sinewave = SineWave(pitch = 0, pitch_per_second = 30)
+signal.signal(signal.SIGINT, signal.default_int_handler)
+
+# Create a sine wave, with a starting pitch of 12, and a pitch change speed of 10/second.
+sinewave = SineWave(pitch = 0, pitch_per_second = int(sys.argv[1]))
 
 # Turn the sine wave on.
 sinewave.play()
 
 while True:
-    with open('/sys/bus/iio/devices/iio:device0/in_illuminance_input', 'r') as file:
+    try:
+        file = open('/sys/bus/iio/devices/iio:device0/in_illuminance_input', 'r')
         data = file.read().rstrip()
-    sinewave.set_pitch(int(data))
+        sinewave.set_pitch(int(data))
+    except KeyboardInterrupt:
+        sinewave.stop()
+        file.close()
+        sys.exit()
